43dd511072dabbd2a205dbf07c927f9877011508
prakat <prakat.dhakall64@gmail.com>
2026-03-07 23:12:25 +0545

    Changed error text color.

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -35,7 +35,7 @@ const NotFoundPage = () => {
       </section>
       <section className="flex-3 bg-gray-100 flex items-center justify-center p-6">
         <div className="text-center max-w-md">
-          <h1 className="text-8xl font-extrabold text-gray-800">404</h1>
+          <h1 className="text-8xl font-extrabold text-black">404</h1>
 
           <h2 className="mt-4 text-2xl font-semibold text-gray-700">
             Page Not Found
